1ccca9062f35b83513540977a1582076471da7c4
Alexander Fish <alexanderfish7@gmail.com>
2017-03-25 01:50:58 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frequency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SoundEmitter wistle = new SoundEmitter();
+            int frequency;
+            int seconds;
+
+            while (true) 
+            {
+                Console.Clear();
+                Console.Write("Sound Frequency (Hertz): ");
+
+                //If frequency input does not successfully convert to an 
+                //int, return to the beginning the the loop to ask again.
+
+                if (Int32.TryParse(Console.ReadLine().Trim(), out frequency) == false)
+                {
+                    continue;
+                }
+                else
+                {
+                    wistle.Hz = frequency;
+                }
+                
+                while (true)
+                {
+                    Console.Clear();
+                    Console.Write("Duration (seconds): ");
+
+                    //If time input successfully converts to an int, break out of loop.
+
+                    if (Int32.TryParse(Console.ReadLine().Trim(), out seconds) != false)
+                    {
+                        wistle.Seconds = seconds;
+                        break;
+                    }
+                }
+
+                Console.Clear();
+                wistle.EmitSound();
+            }
+        }
+    }
+}
---
+++ SoundEmitter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frequency
+{
+    public class SoundEmitter
+    {
+        private static int minHz = 37;
+        private static int maxHz = 32767;
+
+        private int hz;
+        public int Hz
+        {
+            get { return hz; }
+            set
+            {
+                hz = value;
+                if (hz > maxHz)
+                {
+                    hz = maxHz;
+                }
+                else if (hz < minHz)
+                {
+                    hz = minHz;
+                }
+            }
+        }
+
+        private int sec;
+        public int Seconds
+        {
+            get { return sec; }
+            set
+            {
+                sec = value * 1000;
+                if (sec <= 0)
+                {
+                    sec = 1;
+                }
+            }
+        }
+
+        public SoundEmitter() { }
+
+        public SoundEmitter(int frequency, int seconds)
+        {
+            Hz = frequency;
+            Seconds = seconds;
+        }
+
+        public void EmitSound()
+        {
+            Console.WriteLine("{0}Hz", Hz);
+            Console.Beep(Hz, Seconds);
+        }
+    }
+}
